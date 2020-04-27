36a88ef2a2bd747ef9e505e62aeeb1760e3f562c
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-04-28 02:45:47 +0800

    添加room依赖测试通过

--- app/src/main/java/com/winnerwinter/roombasic/MainActivity.java
+++ app/src/main/java/com/winnerwinter/roombasic/MainActivity.java
@@ -1,14 +1,53 @@
 package com.winnerwinter.roombasic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    WordDatabase wordDatabase;
+    WordDao wordDao;
+    TextView textView;
+    Button buttonInsert, buttonUpdate, buttonClear, buttonDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        wordDatabase = Room.databaseBuilder(this, WordDatabase.class, "World database")
+                .allowMainThreadQueries()
+                .build();
+        wordDao = wordDatabase.getWordDao();
+        updateView();
+
+        buttonInsert = findViewById(R.id.buttonInsert);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Word word1 = new Word("Hello", "你好");
+                Word word2 = new Word("World", "世界");
+                wordDao.insertWords(word1, word2);
+                updateView();
+            }
+        });
+
+    }
+
+    void updateView() {
+        List<Word> list = wordDao.getAllWords();
+        String text = "";
+        for (int i = 0; i < list.size(); ++i) {
+            Word word = list.get(i);
+            text += word.getId() + ":" + word.getWord() + "=" + word.getChineseMeaning() + "\n";
+
+        }
+        textView.setText(text);
     }
 }
---
+++ app/src/main/java/com/winnerwinter/roombasic/WordDatabase.java
@@ -0,0 +1,10 @@
+package com.winnerwinter.roombasic;
+
+import androidx.room.Database;
+import androidx.room.Delete;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
+public abstract class WordDatabase extends RoomDatabase {
+    public abstract WordDao getWordDao();
+}
