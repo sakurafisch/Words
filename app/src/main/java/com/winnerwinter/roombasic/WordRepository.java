49d428edd11aa13038cdc874be46f48a52341ed9
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-04-28 20:52:14 +0800

    把WordViewModel的数据库操作API分离到WordRepository

---
+++ app/src/main/java/com/winnerwinter/roombasic/WordRepository.java
@@ -0,0 +1,99 @@
+package com.winnerwinter.roombasic;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class WordRepository {
+    private LiveData<List<Word>> allWordsLive;
+    private WordDao wordDao;
+
+    public WordRepository(Context context) {
+        WordDatabase wordDatabase = WordDatabase.getDatabase(context.getApplicationContext());
+        wordDao = wordDatabase.getWordDao();
+        allWordsLive = wordDao.getAllWordsLive();
+    }
+
+    public LiveData<List<Word>> getAllWordsLive() {
+        return allWordsLive;
+    }
+
+    void insertWords(Word... words) {
+        new InsertAsyncTask(wordDao).execute(words);
+    }
+
+    void updateWords(Word... words) {
+        new UpdateAsyncTask(wordDao).execute(words);
+    }
+
+    void deleteWords(Word... words) {
+        new DeleteAsyncTask(wordDao).execute(words);
+    }
+
+    void deleteAllWords() {
+        new DeleteAllAsyncTask(wordDao).execute();
+    }
+
+    public void setAllWordsLive(LiveData<List<Word>> allWordsLive) {
+        this.allWordsLive = allWordsLive;
+    }
+
+    static class InsertAsyncTask extends AsyncTask<Word, Void, Void> {
+        private WordDao wordDao;
+
+        public InsertAsyncTask(WordDao wordDao) {
+            this.wordDao = wordDao;
+        }
+
+        @Override
+        protected Void doInBackground(Word... words) {
+            wordDao.insertWords(words);
+            return null;
+        }
+    }
+
+    static class UpdateAsyncTask extends AsyncTask<Word, Void, Void> {
+        private WordDao wordDao;
+
+        public UpdateAsyncTask(WordDao wordDao) {
+            this.wordDao = wordDao;
+        }
+
+        @Override
+        protected Void doInBackground(Word... words) {
+            wordDao.updateWords(words);
+            return null;
+        }
+    }
+
+    static class DeleteAsyncTask extends AsyncTask<Word, Void, Void> {
+        private WordDao wordDao;
+
+        public DeleteAsyncTask(WordDao wordDao) {
+            this.wordDao = wordDao;
+        }
+
+        @Override
+        protected Void doInBackground(Word... words) {
+            wordDao.deleteWords(words);
+            return null;
+        }
+    }
+
+    static class DeleteAllAsyncTask extends AsyncTask<Void, Void, Void> {
+        private WordDao wordDao;
+
+        public DeleteAllAsyncTask(WordDao wordDao) {
+            this.wordDao = wordDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            wordDao.deleteAllWords();
+            return null;
+        }
+    }
+}
--- app/src/main/java/com/winnerwinter/roombasic/WordViewModel.java
+++ app/src/main/java/com/winnerwinter/roombasic/WordViewModel.java
@@ -12,91 +12,31 @@ import java.util.List;
 
 public class WordViewModel extends AndroidViewModel {
     private WordDao wordDao;
-    private LiveData<List<Word>> allWordsLive;
+    private WordRepository wordRepository;
     public WordViewModel(@NonNull Application application) {
         super(application);
-        WordDatabase wordDatabase = WordDatabase.getDatabase(application);
-        wordDao = wordDatabase.getWordDao();
-        allWordsLive = wordDao.getAllWordsLive();
+        wordRepository = new WordRepository(application);
+    }
+
+
+    public LiveData<List<Word>> getAllWordsLive() {
+        return wordRepository.getAllWordsLive();
     }
 
     void insertWords(Word... words) {
-        new InsertAsyncTask(wordDao).execute(words);
+        wordRepository.insertWords(words);
     }
 
     void updateWords(Word... words) {
-        new UpdateAsyncTask(wordDao).execute(words);
+        wordRepository.updateWords(words);
     }
 
     void deleteWords(Word... words) {
-        new DeleteAsyncTask(wordDao).execute(words);
+        wordRepository.deleteWords(words);
     }
 
-    void deleteAllWords(Word... words) {
-        new DeleteAllAsyncTask(wordDao).execute();
+    void deleteAllWords() {
+        wordRepository.deleteAllWords();
     }
 
-    public LiveData<List<Word>> getAllWordsLive() {
-        return allWordsLive;
-    }
-
-    public void setAllWordsLive(LiveData<List<Word>> allWordsLive) {
-        this.allWordsLive = allWordsLive;
-    }
-
-    static class InsertAsyncTask extends AsyncTask<Word, Void, Void> {
-        private WordDao wordDao;
-
-        public InsertAsyncTask(WordDao wordDao) {
-            this.wordDao = wordDao;
-        }
-
-        @Override
-        protected Void doInBackground(Word... words) {
-            wordDao.insertWords(words);
-            return null;
-        }
-    }
-
-    static class UpdateAsyncTask extends AsyncTask<Word, Void, Void> {
-        private WordDao wordDao;
-
-        public UpdateAsyncTask(WordDao wordDao) {
-            this.wordDao = wordDao;
-        }
-
-        @Override
-        protected Void doInBackground(Word... words) {
-            wordDao.updateWords(words);
-            return null;
-        }
-    }
-
-    static class DeleteAsyncTask extends AsyncTask<Word, Void, Void> {
-        private WordDao wordDao;
-
-        public DeleteAsyncTask(WordDao wordDao) {
-            this.wordDao = wordDao;
-        }
-
-        @Override
-        protected Void doInBackground(Word... words) {
-            wordDao.deleteWords(words);
-            return null;
-        }
-    }
-
-    static class DeleteAllAsyncTask extends AsyncTask<Void, Void, Void> {
-        private WordDao wordDao;
-
-        public DeleteAllAsyncTask(WordDao wordDao) {
-            this.wordDao = wordDao;
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            wordDao.deleteAllWords();
-            return null;
-        }
-    }
 }
